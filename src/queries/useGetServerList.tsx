44e9c61843ba7c9f1bfcb205131a0ee8f63811b1
nemusona <nemusonaneko@protonmail.com>
2023-09-26 11:18:21 -0500

    fuck cors

--- src/queries/useGetServerList.tsx
+++ src/queries/useGetServerList.tsx
@@ -4,7 +4,7 @@ const API_ROUTE = process.env.NEXT_PUBLIC_API || "http://localhost:3000";
 
 async function getServerList() {
   try {
-    const response = await fetch(`https://publicapi.battlebit.cloud/Servers/GetServerList`, {
+    const response = await fetch(`${API_ROUTE}/api/bbserverlist`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
