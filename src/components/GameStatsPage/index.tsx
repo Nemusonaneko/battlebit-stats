af2bf9212bc2f3164ddeb078c0554e38db872371
nemusona <nemusonaneko@protonmail.com>
2023-09-26 11:09:17 -0500

    revert

--- src/components/GameStatsPage/index.tsx
+++ src/components/GameStatsPage/index.tsx
@@ -5,63 +5,54 @@ import { useEffect, useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 
 export default function GameStatsPage() {
-<<<<<<< HEAD
   const { data, refetch } = useGetServerList();
-  const [currData, setCurrData] = useState<any>(null);
-=======
-  const { data } = useGetServerList();
->>>>>>> parent of f4a7ef7 (refetch?)
   const queryClient = useQueryClient();
   useEffect(() => {
     const interval = setInterval(() => {
-      queryClient.invalidateQueries();
+      refetch();
     }, 1000);
     return () => clearInterval(interval);
-  }, [queryClient]);
-
-  useEffect(() => {
-    setCurrData(data);
-  }, [data]);
+  }, [queryClient, refetch]);
 
   return (
     <div className="p-4">
-      {currData ? (
+      {data ? (
         <div className="flex flex-col justify-center items-center gap-4">
           <div className="flex flex-col justify-center items-center">
             <h1 className="font-bold text-3xl">Total Active Players:</h1>
             <h2 className="text-3xl">
-              {Number(currData.totalActivePlayers).toLocaleString("en-US")}
+              {Number(data.totalActivePlayers).toLocaleString("en-US")}
             </h2>
           </div>
-          {currData.regionData && (
+          {data.regionData && (
             <div className="flex flex-col justify-center items-center gap-2">
               <h1 className="font-bold text-3xl">Regions:</h1>
               <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 justify-center items-center">
-                {Object.keys(currData.regionData)
+                {Object.keys(data.regionData)
                   .sort()
                   .map((region: string, i: number) => (
                     <RegionCard
                       key={i}
                       title={region}
-                      players={currData.regionData[region].activePlayers}
+                      players={data.regionData[region].activePlayers}
                     />
                   ))}
               </div>
             </div>
           )}
-          {currData.gameModeData && (
+          {data.gameModeData && (
             <div className="flex flex-col justify-center items-center gap-2">
               <h1 className="font-bold text-3xl">Game Modes:</h1>
               <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center">
-                {Object.keys(currData.gameModeData)
+                {Object.keys(data.gameModeData)
                   .sort()
                   .map(
                     (mode: string, i: number) =>
-                      currData.gameModeData[mode].activePlayers > 0 && (
+                      data.gameModeData[mode].activePlayers > 0 && (
                         <RegionCard
                           key={i}
                           title={mode}
-                          players={currData.gameModeData[mode].activePlayers}
+                          players={data.gameModeData[mode].activePlayers}
                         />
                       )
                   )}
--- src/components/ServerListPage/index.tsx
+++ src/components/ServerListPage/index.tsx
@@ -4,37 +4,27 @@ import ServerListTable from "../Table/ServerList";
 import { serverListColumns } from "../Table/ServerList/columns";
 import { Card, CardContent, CardTitle } from "../ui/card";
 import { Button } from "../ui/button";
-import { useQueryClient } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 
 export default function ServerListPage() {
-<<<<<<< HEAD
   const { data, refetch } = useGetServerList();
-  const [currData, setCurrData] = useState<any>(null);
-=======
-  const { data } = useGetServerList();
->>>>>>> parent of f4a7ef7 (refetch?)
-  const queryClient = useQueryClient();
-
-  useEffect(() => {
-    setCurrData(data);
-  }, [data]);
 
   return (
     <div className="p-4">
-      {currData ? (
+      {data ? (
         <Card>
           <CardTitle className="p-4 flex items-center gap-2">
             <h4 className="font-bold text-2xl">Server List</h4>
             <Button
               className="text-xs hover:bg-gray-500"
-              onClick={() => queryClient.invalidateQueries()}
+              onClick={() => {
+                refetch();
+              }}
             >
               Refresh
             </Button>
           </CardTitle>
           <CardContent>
-            <ServerListTable data={currData.data} columns={serverListColumns} />
+            <ServerListTable data={data.data} columns={serverListColumns} />
           </CardContent>
         </Card>
       ) : (
